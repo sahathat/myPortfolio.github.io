773472c5d6c866849618fc3608081de52192f146
sahathat <61311677+sahathat@users.noreply.github.com>
2024-06-30 19:20:07 +0700

    Portfolio version 1

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -40,8 +40,8 @@ export default {
 			</p>
 			<div class="flex justify-center sm:block">
 				<a
-					download="Sahathat-YingsakulkietV3.pdf"
-					href="/Sahathat-YingSakulkietV3.pdf"
+					download="Sahathat-YingSakulkietV3.pdf"
+					href="/files/Sahathat-YingSakulkietV3.pdf"
 					class="flex justify-center items-center w-auto sm:w-64 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 					aria-label="Download Resume/CV"
 				>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.(pdf)$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: `files/[name].[ext]`
+                    }
+                }
+            ],
+        },
+    },
     publicPath: process.env.NODE_ENV === 'production'
-      ? './'
-      : './'
-  }
+        ? './'
+        : './'
+}
