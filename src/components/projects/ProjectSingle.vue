b2853f8cb476476b8b736a76eae58d7044c3170f
sahathat <61311677+sahathat@users.noreply.github.com>
2024-06-30 18:45:20 +0700

    Portfolio version 1

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-	<router-link :to="'/projects/single-project/' + project.id"
+	<router-link :to="'/projects/' + project.id"
 		class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
 		aria-label="Single Project">
 		<div class="justify-center">
