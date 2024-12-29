c9d024c61372e88752f418198c476a56f1e09066
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2024-12-29 23:51:30 +0700

    Update ProjectRelatedProjects.vue

--- src/components/projects/ProjectRelatedProjects.vue
+++ src/components/projects/ProjectRelatedProjects.vue
@@ -1,6 +1,15 @@
 <script>
 export default {
 	props: ['relatedProject'],
+	methods: {
+		redirectToItem(id) {
+			// Redirect to the item link using Vue Router's programmatic navigation
+			this.$router.push('/' + id).then(() => {
+				// Force a full page reload
+				window.location.reload();
+			});
+		}
+	}
 };
 
 </script>
@@ -14,10 +23,8 @@ export default {
 
 		<div class="grid grid-cols-1 sm:grid-cols-4 gap-10">
 			<div v-for="item in relatedProject.relatedProjects" :key="item.id">
-				<router-link :to="'/' + item.id" >
-					<img v-lazy="item.img" class="rounded-xl cursor-pointer"
-						:alt="item.title" :title="item.title" /> <p class="font-general-regular text-ternary-dark dark:text-ternary-light">{{ item.title }}</p>
-				</router-link>
+				<img v-lazy="item.img" class="rounded-xl cursor-pointer" @click="redirectToItem(item.id)"
+					:alt="item.title" :title="item.title" /> <p class="font-general-regular text-ternary-dark dark:text-ternary-light">{{ item.title }}</p>
 			</div>
 		</div>
 	</div>
